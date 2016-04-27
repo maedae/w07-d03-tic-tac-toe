1d8b525d2ed2b07c6dac379d346b872454e742af
Peter Camp <Pete.f.Camp@gmail.com>
2016-04-27 14:43:13 -0500

    1st

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 window.addEventListener("load", setUpPage);
 
-function setUpPage () {
-
+function setUpPage() {
+		var tiles = document.getElementsByClassName("board-tile");
+		for (var i = 0; i < tiles.length; i++ ){
+			tiles[i].addEventListener("click", changeToX())
+		}
+}
   // initialize whatever variables you need and add your event listeners
   // you can (but probably shouldn't) define functions inside this function
 
-};
+
+	function changeToX() {
+		this.innerHTML = "X";
+	} 
+	
